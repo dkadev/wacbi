69f8d1838e32f0de2edf6eb8a2d6b98dc2437411
dkadev <davidcomeron19@gmail.com>
2024-06-10 01:58:40 +0200

    chore: Add cors dependency and enable CORS in Express API server

--- api/server.js
+++ api/server.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+const express = require("express")
+const cors = require("cors")
 
-const routes = require('./routes');
+const app = express()
+const port = 8080
 
-app.use(express.json());
-app.use('/api', routes);
+const routes = require("./routes")
+
+const corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+app.use("/api", routes)
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+    console.log(`Server is running on http://localhost:${port}`)
+})
