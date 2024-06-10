4cf7bb909a7d14f97806859b090247852405f8bf
dkadev <23488064+dkadev@users.noreply.github.com>
2024-06-11 00:48:25 +0200

    fix: chats fetching

--- app/src/components/chat/ChatList.tsx
+++ app/src/components/chat/ChatList.tsx
@@ -1,18 +1,22 @@
+'use client'
+
 import { Chat, columns } from "@/components/chat/ChatModel"
 import { ChatTable } from "@/components/chat/ChatTable"
+import { useEffect, useState } from 'react';
 
 async function getChats(): Promise<Chat[]> {
     const response = await fetch("http://localhost:8080/api/chats")
     const data = await response.json()
-
     console.log(data)
-
     return data
 }
 
-export default async function ChatList() {
-    const chats = await getChats()
+export default function ChatList() {
+    const [chats, setChats] = useState<Chat[]>([]);
+
+    useEffect(() => {
+        getChats().then(data => setChats(data));
+    }, []);
 
     return <ChatTable columns={columns} data={chats} />
 }
-
